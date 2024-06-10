d26f9a88b6ecb49f54e8d9f1b4ef4d45dde3d8fd
Ranush Mithila <36297308+RanushMithila@users.noreply.github.com>
2024-06-10 10:49:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 @app.route('/greet')
 def greet():
     name = request.args.get('name', 'World')  # Default to 'World' if no name is provided
-    name_parts = name.split(" ")
+    # name_parts = name.split(" ")
     
     # Ensure the command is a flat list of strings
-    if len(name_parts) > 1:
-        command = name_parts
-    else:
-        command = [name]
+    # if len(name_parts) > 1:
+    #     command = name_parts
+    # else:
+    #     command = [name]
     
     try:
-        result = subprocess.run(command, capture_output=True, text=True)
+        result = subprocess.run(command, capture_output=True, text=True, shell=True)
         return result.stdout
     except Exception as e:
         return f'Error executing command: {str(e)}'
